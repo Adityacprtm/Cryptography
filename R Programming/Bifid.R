e0bfcae283d5ec2c53df4724201223a7f99f3afa
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-09-22 15:53:19 +0700

    Update

--- R Programming/Bifid.R
+++ R Programming/Bifid.R
@@ -1,42 +1,69 @@
-bifid <- function() {
-	cat("input msg: ")
-	str <- readline()
-	alpha <- c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y/Z")
-	M <- matrix(alpha,nrow=5,ncol=5,byrow=TRUE)
-	print(M)
-	upper_str <- toupper(gsub(" ","",str))
-	num_str <- nchar(upper_str)
-	cat(sprintf("Plaintext> %s \n",gsub(" ","",str)))
-	row <- " "
-	col <- " "
-	for (i in 1:num_str){
-		pos <- substr(upper_str,i,i)
-		if (pos == "Y" || pos == "Z"){
+get_cipher <- function(input, M){
+	cipher <- NULL
+	pos <- strsplit(input,"")
+	for (i in 1:nchar(input)){
+		# lookin for i odd
+		if (i%%2==1){
+			row <- as.numeric(pos[[1]][i])
+			col <- as.numeric(pos[[1]][i+1])
+			if (M[row,col] == "Y/Z"){
+				x <- paste("[",M[row,col],"]")
+				cipher <- paste(cipher,x)
+			} else {
+				cipher <- paste(cipher,M[row,col])
+			}
+		}
+	}
+	return(gsub(" ","",cipher))
+}
+
+encrypt <- function(input,M){
+	temp_cipher <- NULL
+	row <- NULL
+	col <- NULL
+	pos <- strsplit(input,"")
+	for (i in 1:(nchar(input))){
+		if (pos[[1]][i] == "Y" || pos[[1]][i] == "Z"){
 			row <- paste(row,5)
 			col <- paste(col,5)
 		}
 		for (j in 1:5){
 			for (k in 1:5){
-				if (pos == M[j,k]){
+				if (pos[[1]][i] == M[j,k]){
 					row <- paste(row,j)
 					col <- paste(col,k)
 				} 
 			}
 		}
 	}
-	row <- gsub(" ","",row)
-	col <- gsub(" ","",col)
-	cat(sprintf("Row> %s\nCol> %s\n",row,col))
-	x <- gsub(" ","",paste(row,col))
-	num_row_col <- nchar(x)
-	pos <- strsplit(x,"")
-	m <- matrix(unlist(pos),nrow=1)
-	print(m)
-	cipher <- ""
-	for (i in 1:num_row_col){
-		cipher <- paste(M[as.numeric(m[1,i]),as.numeric(m[1,i])])
-		print(cipher)
-	}
-	cat(sprintf("Cipher> %s\n",x))
+	temp_cipher <- gsub(" ","",paste(row,col))
+	cat(sprintf("Row> %s\nCol> %s\nRow-Col> %s\n",row,col,temp_cipher))
+
+	#get index from bifid table
+	cipher <- get_cipher(temp_cipher,M)
+	return(cipher)
 }
+
+bifid <- function() {
+	# Inisialisasi table matrix polybius
+	M <- matrix(c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y/Z"),nrow=5,ncol=5,byrow=TRUE)
+	
+	# Input Plaintext
+	cat("INPUT: ")
+	input <- readline()
+	input <- toupper(gsub(" ","",input))
+	
+	# print table
+	cat(sprintf("Bifid Table\n"))
+	print(M)
+	
+	# Print Plaintext
+	cat(sprintf("Plaintext> %s\n",input))
+	
+	# Encrypt & Print Ciphertext
+	cat(sprintf("Encrypt now ...\n"))
+	cipher <- encrypt(input,M)
+	cat(sprintf("Ciphertext> %s\n",cipher))
+}
+
 bifid()
--- R Programming/Polybius.R
+++ R Programming/Polybius.R
@@ -1,26 +1,69 @@
-polybius <- function() {
-	cat("input msg: ")
-	str <- readline()
-	alpha <- c("A","B","C","D","E","F","G","H","I/J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
-	M = matrix(alpha,nrow=5,ncol=5,byrow=TRUE)
-	print(M)
-	upper_str = toupper(gsub(" ","",str))
-	num_str = nchar(upper_str)
-	cat(sprintf("Plaintext> %s \nCipher> ",gsub(" ","",str)))
-	cipher <- " "
-	for (i in 1:num_str){
-		pos = substr(upper_str,i,i)
-		if (pos == "J" || pos == "I"){
+encrypt <- function(input,M){
+	cipher <- NULL
+	# split per char
+	pos <- strsplit(input,"")
+	for (i in 1:nchar(input)){
+		if (pos[[1]][i] == "J" || pos[[1]][i] == "I"){
 			cipher <- paste(cipher,2,4)
 		}
 		for (j in 1:5){
 			for (k in 1:5){
-				if (pos == M[j,k]){
+				if (pos[[1]][i] == M[j,k]){
 					cipher <- paste(cipher,j,k)
-				} 
+				}
 			}
 		}
 	}
-	cat(sprintf("%s\n",gsub(" ","",cipher)))
+	# retutn cipher without space
+	return(gsub(" ","",cipher))
 }
+
+decrypt <- function(cipher,M){
+	plain <- NULL
+	# split per char
+	pos <- strsplit(cipher,"")
+	for (i in 1:nchar(cipher)){
+		# lookin for i odd
+		if (i%%2==1){
+			row <- as.numeric(pos[[1]][i])
+			col <- as.numeric(pos[[1]][i+1])
+			if (M[row,col] == "I/J"){
+				x <- paste("[",M[row,col],"]")
+				plain <- paste(plain,x)
+			}else {
+				plain <- paste(plain,M[row,col])
+			}
+		}
+	}
+	# retutn plain without space
+	return(gsub(" ","",plain))
+}
+
+polybius <- function() {
+	# Inisialisasi table matrix polybius
+	M <- matrix(c("A","B","C","D","E","F","G","H","I/J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"),nrow=5,ncol=5,byrow=TRUE)
+	
+	# Input Plaintext
+	cat("INPUT: ")
+	input <- readline()
+	input <- toupper(gsub(" ","",input))
+	
+	# print table
+	cat(sprintf("Polybius Table\n"))
+	print(M)
+	
+	# Print Plaintext
+	cat(sprintf("Plaintext> %s\n",input))
+	
+	# Enkrip & Print Ciphertext
+	cat(sprintf("Encrypt now ...\n"))
+	cipher <- encrypt(input,M)
+	cat(sprintf("Ciphertext> %s\n",cipher))
+	
+	# Dekrip & Print Plaintext
+	cat(sprintf("Decrypt now ...\n"))
+	plain <- decrypt(cipher,M)
+	cat(sprintf("Plaintext> %s\n",plain))
+}
+
 polybius()
