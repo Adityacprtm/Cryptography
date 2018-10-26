64ffcb6079f4dec6e9b20d231d71bcec382c3dde
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-10-26 08:28:14 +0700

    Update

--- R Programming/Blockchain/1/client.R
+++ R Programming/Blockchain/1/client.R
@@ -4,35 +4,35 @@ library(httr)
 # to register a node
 req <- POST("http://127.0.0.1:8000/nodes/register", 
             body = '{"nodes": "http://127.0.0.1:8000"}')
-cat(jsonlite::fromJSON(content(req, "text", encoding = "utf-8")))
+cat(jsonlite::fromJSON(content(req, "text", encoding = "utf-8")),"\n")
 
 # create a new transaction
 req <- POST("http://127.0.0.1:8000/transactions/new", 
             body = '{"sender": "d4ee26eee15148ee92c6cd394edd964", 
             "recipient": "23448ee92cd4ee26eee6cd394edd964", "amount": 1}')
 object <- jsonlite::fromJSON(content(req, "text", encoding = "utf-8"));
-object$message
+cat(object$message,"\n")
 
 # create a new transaction
 req <- POST("http://127.0.0.1:8000/transactions/new", 
             body = '{"sender": "6eee15148ee92c6cd394edd974d4ee2", 
             "recipient": "15148ee92cd4ee26eee6cd394edd964", "amount": 2}')
 object <- jsonlite::fromJSON(content(req, "text", encoding = "utf-8"));
-object$message
+cat(object$message,"\n")
 
 # start mining
 req <- GET("http://127.0.0.1:8000/mine")
 object <- jsonlite::fromJSON(content(req, "text", encoding = "utf-8"));
-object$message
+cat(object$message,"\n")
 
 # create a new transaction
 req <- POST("http://127.0.0.1:8000/transactions/new", 
             body = '{"sender": "334e15148ee92c6cd394edd974d4ee2", 
             "recipient": "8ee98ee92cd4ee26eee6cd3334e1514", "amount": 11}')
 object <- jsonlite::fromJSON(content(req, "text", encoding = "utf-8"));
-object$message
+cat(object$message,"\n")
 
 # mine again
 req <- GET("http://127.0.0.1:8000/mine")
 object <- jsonlite::fromJSON(content(req, "text", encoding = "utf-8"));
-object$message
+cat(object$message,"\n")
