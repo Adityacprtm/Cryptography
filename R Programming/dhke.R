be48ee82916baf2d426e3408ceae7199cc20ec3f
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-11-01 00:03:45 +0700

    update

--- R Programming/dhke.R
+++ R Programming/dhke.R
@@ -15,53 +15,48 @@ setup <- function(p,g,x) {
     p <- as.numeric(p)
     g <- as.numeric(g)
     x <- as.numeric(x)
-
     # is prime?
     if (!(isPrime(p))){
         stop("p must be prime")
     }
-
     # g must be smaller than p
     if (g > p) {
         stop("g must be smaller than p")
     }
-
+    # x must be int and > 0
     if (x < 0) {
         stop("Secret key (X) must be integers greater than 0")
     }
-
     # Calculated with formula
     y <- (g^x) %% p
-
     return(y)
 }
 
 main <- function() {
+    # enter number as P and G
     cat("Enter a prime number as p: ")
     p <- readline()
     cat("Enter a number as g: ")
     g <- readline()
     cat("\n")
-
+    # enter secret key Alice and Bob
     cat("Alice Scret Key: ")
     xa <- readline()
     cat("Bob Secret Key: ")
     xb <- readline()
     cat("\n")
-
+    # Publicly Shared
     cat("Publicly Shared Variables:\n")
     ya <- setup(p,g,xa)
     cat(sprintf("   Alice Sends to Bob Over Public Chanel: %s\n",ya))
     yb <- setup(p,g,xb)
     cat(sprintf("   Bob Sends to Alice Over Public Chanel: %s\n",yb))
-
     cat("\n----------------------------------\n\n")
-
+    # Shared Key
     cat("Privately Calculated Shared Secret:\n")
     aliceSharedKey <- setup(p,yb,xa)
     bobSharedKey <- setup(p,ya,xb)
     cat(sprintf("   Alice Shared Key: %s\n",aliceSharedKey))
     cat(sprintf("   Bob Shared Key: %s\n",bobSharedKey))
 }
-
 main()
